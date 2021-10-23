93c2127165ee1ba6dbbcc603979d272399d85558
akirato0223 <akiraling0223@gmail.com>
2021-10-23 19:59:11 -0400

    change file

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 
-import Home from './Components/Pages/Home'
 import ImportRepo from  './Components/Pages/ImportRepo'
 import Plagarism from './Components/Pages/Plagarism'
 import BaseNav from './Components/Pages/BaseNav'
@@ -33,8 +32,7 @@ const Routes: React.FC = () => {
   return (
     <HashRouter>
       <Switch>
-        <Route path="/" exact render={() => <Home />}></Route>
-        <Route path="/import_repo" exact render={() => <ImportRepo getData={getData}/>}></Route>
+        <Route path="/" exact render={() => <ImportRepo getData={getData}/>}></Route>
         <Route path="/plagarism" exact render={() => <BaseNav><Plagarism /></BaseNav>}></Route>
         <Route path="/settings" exact render={() => <BaseNav><Settings /></BaseNav>}></Route>
       </Switch>
