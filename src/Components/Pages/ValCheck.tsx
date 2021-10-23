d5c7061e9800d4e0561ea5c233d1064011c1c469
Sean Roades <seanroades@college.harvard.edu>
2021-10-23 13:50:00 -0400

    valcheck added

---
+++ src/Components/Pages/ValCheck.tsx
@@ -0,0 +1,15 @@
+import '../../App.css';
+
+function ValCheck() {
+    /* 
+    TODO just a basic loading message
+    TODO create animation
+    */
+    return (
+      <div className="App">
+        <p className="BasicTitle">Returned List</p>
+      </div>
+    );
+  }
+  
+  export default ValCheck;
