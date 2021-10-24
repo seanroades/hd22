56f5b0bda19a85699325154e6e45dc1e9da2bae4
elliekkim <ellie.k.kim@gmail.com>
2021-10-23 20:13:42 -0400

    UI update 3

--- src/Components/Pages/ImportRepo.tsx
+++ src/Components/Pages/ImportRepo.tsx
@@ -15,34 +15,39 @@ const ImportRepo = (props: ImportProps) => {
       <Container maxWidth="sm">
         {/* <div className={styles.wrapper}> */}
 
-        <Grid item p={2}>
-          <Typography>Enter hackathon start date:</Typography>
-        </Grid>
+        
+      <Grid item p={2}>
+        <Typography>Enter hackathon start date:</Typography> 
+      </Grid>
 
-        <Grid item p={2}>
-          <TextField
-            fullWidth
-            label="MM-DD-YY"
-            multiline
-            maxRows={4}
-            id="fullWidth"
-            InputProps={{
-              className: styles.input,
-            }}
-          />
-        </Grid>
-        <Grid container direction="row" spacing="8">
-          <Grid item p={2}>
-            <Button
-              variant="contained"
-              color="primary"
-              className={styles.button}
-              size="large"
-              onClick={props.getData}
-            >
-              Submit
-            </Button>
-          </Grid>
+      <Grid item p={2}>
+        <TextField fullWidth label="MM-DD-YY" multiline maxRows={4} id="fullWidth" InputProps={{
+              className: styles.input
+            }}/>
+      </Grid>
+
+      <Grid item p={2}>
+        <Typography>Enter list of GitHub project links: </Typography> 
+      </Grid>
+
+      <Grid item p={2}>
+        <TextField fullWidth label="Enter your GitHub lists here, separated by line" multiline maxRows={4} id="fullWidth" InputProps={{
+              className: styles.input
+            }}/>
+      </Grid>
+
+      <Grid container direction="row" spacing="8">
+      <Grid item p={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={styles.button}
+            size="large"
+            onChange={props.getData}
+          >
+            Submit
+          </Button>
+      </Grid>
         </Grid>
         {/* </div> */}
       </Container>
